8949095108cc258541565fdefb2aa0bd97a29e81
AustinNeverPee <austin_never_pee@foxmail.com>
2017-04-04 16:12:07 +0800

    Init

---
+++ RecipePage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=234238 上有介绍
+
+namespace 且行且烹饪
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class RecipePage : Page
+    {
+        public RecipePage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            //To Do
+            //this.Frame.Navigate(typeof());
+        }
+
+        private void Forward_Click(object sender, RoutedEventArgs e)
+        {
+            //To Do
+            //this.Frame.Navigate(typeof());
+        }
+
+        private void Home_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage));
+        }
+    }
+}
